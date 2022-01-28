67c2590fc4a3d03783bb285753cbd759db3503a0
Shrey Patel <patelshrey77@gmail.com>
2022-01-28 12:47:27 +0530

    Algo update

--- src/main/java/Fidoop/Project.java
+++ src/main/java/Fidoop/Project.java
@@ -1,13 +1,175 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package Fidoop;
+import java.io.*;
+import java.util.*;
 
-/**
- *
- * @author nan
- */
-public class Project {
+public class Apriori extends Observable {
+    public static void main(String[] args) throws Exception {
+        Apriori ap = new Apriori(args);
+    }
+    private List<int[]> itemsets ;
+    /** the name of the transcation file */
+    private String transaFile; 
+    /** number of different items in the dataset */
+    private int numItems; 
+    /** total number of transactions in transaFile */
+    private int numTransactions; 
+    /** minimum support for a frequent itemset in percentage, e.g. 0.8 */
+    private double minSup; 
     
-}
+    /** by default, Apriori is used with the command line interface */
+    private boolean usedAsLibrary = false;
+
+    /** This is the main interface to use this class as a library */
+    public  Apriori(String[] args, Observer ob) throws Exception
+    {
+    	usedAsLibrary = true;
+    	configure(args);
+    	this.addObserver(ob);
+    	go();
+    }
+    public  Apriori(String[] args) throws Exception
+    {
+        configure(args);
+        go();
+    }
+
+    /** starts the algorithm after configuration */
+    private void go() throws Exception {
+        //start timer
+        long start = System.currentTimeMillis();
+
+        // first we generate the candidates of size 1
+        createItemsetsOfSize1();        
+        int itemsetNumber=1; //the current itemset being looked at
+        int nbFrequentSets=0;
+        
+        while (itemsets.size()>0)
+        {
+
+            calculateFrequentItemsets();
+
+            if(itemsets.size()!=0)
+            {
+                nbFrequentSets+=itemsets.size();
+                log("Found "+itemsets.size()+" frequent itemsets of size " + itemsetNumber + " (with support "+(minSup*100)+"%)");;
+                createNewItemsetsFromPreviousOnes();
+            }
+
+            itemsetNumber++;
+        }
+
+    /** triggers actions if a frequent item set has been found  */
+    private void foundFrequentItemSet(int[] itemset, int support) {
+    	if (usedAsLibrary) {
+            this.setChanged();
+            notifyObservers(itemset);
+    	}
+    	else {System.out.println(Arrays.toString(itemset) + "  ("+ ((support / (double) numTransactions))+" "+support+")");}
+    }
+
+    /** outputs a message in Sys.err if not used as library */
+    private void log(String message) {
+    	if (!usedAsLibrary) {
+    		System.err.println(message);
+    	}
+    }
+
+    /** computes numItems, numTransactions, and sets minSup */
+    private void configure(String[] args) throws Exception
+    {        
+        // setting transafile
+        if (args.length!=0) transaFile = args[0];
+        else transaFile = "chess.dat"; // default
+    	
+    	// setting minsupport
+    	if (args.length>=2) minSup=(Double.valueOf(args[1]).doubleValue());    	
+    	else minSup = .8;// by default
+    	if (minSup>1 || minSup<0) throw new Exception("minSup: bad value");
+    
+    	numItems = 0;
+    	numTransactions=0;
+    	BufferedReader data_in = new BufferedReader(new FileReader(transaFile));
+    	while (data_in.ready()) {    		
+    		String line=data_in.readLine();
+    		if (line.matches("\\s*")) continue; // be friendly with empty lines
+    		numTransactions++;
+    		StringTokenizer t = new StringTokenizer(line," ");
+    		while (t.hasMoreTokens()) {
+    			int x = Integer.parseInt(t.nextToken());
+    			//log(x);
+    			if (x+1>numItems) numItems=x+1;
+    		}    		
+    	}  
+    	
+        outputConfig();
+
+    }
+	private void outputConfig() {
+		//output config info to the user
+		 log("Input configuration: "+numItems+" items, "+numTransactions+" transactions, ");
+		 log("minsup = "+minSup*100+"%");
+	}
+	private void createItemsetsOfSize1() {
+		itemsets = new ArrayList<int[]>();
+        for(int i=0; i<numItems; i++)
+        {
+        	int[] cand = {i};
+        	itemsets.add(cand);
+        }
+	}
+    private void createNewItemsetsFromPreviousOnes()
+    {
+    	int currentSizeOfItemsets = itemsets.get(0).length;
+    	log("Creating itemsets of size "+(currentSizeOfItemsets+1)+" based on "+itemsets.size()+" itemsets of size "+currentSizeOfItemsets);
+    		
+    	HashMap<String, int[]> tempCandidates = new HashMap<String, int[]>(); //temporary candidates
+    	
+        for(int i=0; i<itemsets.size(); i++)
+        {
+            for(int j=i+1; j<itemsets.size(); j++)
+            {
+                int[] X = itemsets.get(i);
+                int[] Y = itemsets.get(j);
+
+                assert (X.length==Y.length);
+                
+                //make a string of the first n-2 tokens of the strings
+                int [] newCand = new int[currentSizeOfItemsets+1];
+                for(int s=0; s<newCand.length-1; s++) {
+                	newCand[s] = X[s];
+                }
+                    
+                int ndifferent = 0;
+                // then we find the missing value
+                for(int s1=0; s1<Y.length; s1++)
+                {
+                	boolean found = false;
+                	// is Y[s1] in X?
+                    for(int s2=0; s2<X.length; s2++) {
+                    	if (X[s2]==Y[s1]) { 
+                    		found = true;
+                    		break;
+                    	}
+                	}
+                	if (!found){ // Y[s1] is not in X
+                		ndifferent++;
+                		// we put the missing value at the end of newCand
+                		newCand[newCand.length -1] = Y[s1];
+                	}
+            	
+            	}
+                assert(ndifferent>0);
+                
+                
+                if (ndifferent==1) {
+           	Arrays.sort(newCand);
+                	tempCandidates.put(Arrays.toString(newCand),newCand);
+                }
+            }
+        }
+        
+        //set the new itemsets
+        itemsets = new ArrayList<int[]>(tempCandidates.values());
+    	log("Created "+itemsets.size()+" unique itemsets of size "+(currentSizeOfItemsets+1));
+
+    }
+
